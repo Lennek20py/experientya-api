c41305cdf6a9e4c038ea74751f8807e3a594181f
Ulises Rodríguez <ulisesmercer9@gmail.com>
2022-06-18 14:14:41 -0500

    feat: add to inertia js to project

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+});
+</script>
+
+<template>
+    <div>
+        <h1>hola</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
