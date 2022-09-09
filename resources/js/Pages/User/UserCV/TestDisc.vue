1058ea8d3cf8006ba613b349d5feb39e1b7df7ff
HUGO ANAYA <hugo.anaya@live.com>
2022-09-09 09:14:50 -0500

    feat: mobile and desktop design for TestDisc

--- resources/js/Pages/User/UserCV/TestDisc.vue
+++ resources/js/Pages/User/UserCV/TestDisc.vue
@@ -8,8 +8,7 @@
         <div class="h-2 bg-gray-200 rounded-full max-w-[1150px] mb-2.5"></div>
         <div class="h-2 bg-gray-200 rounded-full max-w-[1130px] mb-2.5"></div>
         <div class="h-2 bg-gray-200 rounded-full max-w-[260px] mb-5"></div>
-        <!-- <div class="h-2 bg-gray-200 rounded-full max-w-[260px] mb-2.5"></div> -->
-        <div class="flex mx-auto justify-center align-middle items-center w-full h-32 bg-gray-300 rounded sm:w-48">
+        <div class="flex mx-auto justify-center align-middle items-center w-1/2 h-32 bg-gray-300 rounded">
           <svg class="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/></svg>
         </div>
         <span class="sr-only">Loading...</span>
@@ -20,9 +19,9 @@
         </span>
         <svg class="py-4 self-center w-[100px] lg:w-[120px] text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
         <div @click="toggleModal()" class="w-auto flex justify-center items-center lg:justify-center lg:py-1">
-            <a v-if="ifExists" class="flex items-center cursor-pointer content-center justify-cente text-lg font-semibold text-start rounded-full bg-red-500 text-gray-50 px-4 py-1">
+            <a v-if="ifExists" class="flex items-center cursor-pointer content-center justify-cente text-lg font-semibold text-start rounded-full bg-red-600 text-gray-50 px-4 py-1">
             CONTINUAR</a>
-            <a v-else class="flex items-center cursor-pointer content-center justify-cente text-lg font-semibold text-start rounded-full bg-red-500 text-gray-50 px-4 py-1">
+            <a v-else class="flex items-center cursor-pointer content-center justify-cente text-lg font-semibold text-start rounded-full bg-red-600 text-gray-50 px-4 py-1">
             INICIAR TEST</a>
         </div>
     </div>
@@ -48,34 +47,34 @@
         <div class="mt-3 w-full text-center sm:mt-0 sm:ml-4 sm:text-left overflow-auto ...">
           <div class="flex text-center flex-col justify-center items-center mb-4 w-full">
                 <!-- TÍTULO -->
-            <h3 class="uppercase px-1 py-2 text-xl text-center leading-6 font-bold text-gray-900" id="modal-headline">
+            <h3 class="uppercase px-1 py-2 text-xl text-center leading-6 font-bold text-gray-900 lg:text-3xl" id="modal-headline">
               TEST DE COMPETENCIAS
             </h3>
                 <!-- INSTRUCCIONES -->
-            <p class="text-xs text-gray-500 text-justify border-b-2">Lea atentamente el enunciado y seleccione la opción que más se acople a su manera de ser. Si desea cerrar la ventana del test, este se guardará en la última respuesta brindada; le recomendamos tomar el test sin distractores externos y en un solo intento.</p>
+            <p class="text-xs text-gray-500 text-justify border-b-2 sm:mx-10 md:mx-20 lg:text-base">Lea atentamente el enunciado y seleccione la opción que más se acople a su manera de ser. Si desea cerrar la ventana del test, este se guardará en la última respuesta brindada; le recomendamos tomar el test sin distractores externos y en un solo intento.</p>
           </div>
             <!-- CUERPO -->
-          <div class="mt-2 flex flex-col modal-body">
-            <div class="self-start px-1 py-2 text- leading-6 font-bold text-gray-900">Instrucciones:</div>
+          <div class="mt-2 flex flex-col modal-body px-0 sm:px-10 md:px-20">
+            <div class="self-start px-1 py-2 leading-6 font-bold text-gray-900 lg:text-lg">Instrucciones:</div>
             <!-- TEXTO DE INFORMACIÓN  -->
-            <div class="flex flex-col">
+            <div class="flex flex-col lg:text-base">
                 <div class="py-1">
-                    <p class="text-xs text-gray-500 text-justify leading-none justify-start">1- El test te mostrará cuatro palabras en pantalla. Est le pedirá que seleccione primero la palabra con la que más se sienta identificado.</p>
+                    <p class="text-xs text-gray-500 text-justify leading-none justify-start lg:text-base">1- El test te mostrará cuatro palabras en pantalla. Est le pedirá que seleccione primero la palabra con la que más se sienta identificado.</p>
 
                     <img class="w-56 h-auto mx-auto py-2 mb-4" src="../../../../img/Instruction1.png" alt="">
                 </div>
                 <div class="py-1">
-                    <p class="text-xs text-gray-500 text-justify leading-none justify-start">2- Para elegir el ítem solicitado, solo basta con darle clic en cualquier parte del cuadrado y se pintará del color acorde a lo solicitado, verde para "más semejante" y rojo para "menos semejante".</p>
+                    <p class="text-xs text-gray-500 text-justify leading-none justify-start lg:text-base">2- Para elegir el ítem solicitado, solo basta con darle clic en cualquier parte del cuadrado y se pintará del color acorde a lo solicitado, verde para "más semejante" y rojo para "menos semejante".</p>
 
                     <img class="w-56 h-auto mx-auto py-2  mb-4" src="../../../../img/Instruction2.png" alt="">
                 </div>
                 <div class="py-1">
-                    <p class="text-xs text-gray-500 text-justify leading-none justify-start">3- Si las elecciones son correctas, proceda a darle clic en el botón azul de la esquina inferior derecha para pasar al siguiente conjunto de palabras.</p>
+                    <p class="text-xs text-gray-500 text-justify leading-none justify-start lg:text-base">3- Si las elecciones son correctas, proceda a darle clic en el botón azul de la esquina inferior derecha para pasar al siguiente conjunto de palabras.</p>
 
                     <img class="w-56 h-auto mx-auto py-2  mb-4" src="../../../../img/Instruction2.png" alt="">
                 </div>
                 <div class="py-1">
-                    <p class="text-xs text-gray-500 text-justify leading-none justify-start">4- Al pasar todas las preguntas el test tendrá una pantalla de finalizado, esta mostará el estado del test y si se realizó de manera correcta.</p>
+                    <p class="text-xs text-gray-500 text-justify leading-none justify-start lg:text-base">4- Al pasar todas las preguntas el test tendrá una pantalla de finalizado, esta mostará el estado del test y si se realizó de manera correcta.</p>
                 </div>
             </div>
             <!-- BOTONES DEE RESPUESTA -->
@@ -87,7 +86,7 @@
         <!-- BOTONES / FOOTER PAGE -->
     <div class="bg-gray-50 gap-2 px-4 py-6 sm:px-6 flex flex-col sm:flex-row-reverse">
       <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-        <button @click="toggleModalTest(), toggleModal()" type="button" class="inline-flex justify-center font-bold w-full rounded-md select-none border border-gray-300 px-4 py-2 bg-red-500 text-base leading-6 text-white shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+        <button @click="toggleModalTest(), toggleModal()" type="button" class="inline-flex justify-center font-bold w-full rounded-md select-none border border-gray-300 px-4 py-2 bg-red-600 text-base leading-6 text-white shadow-sm hover:text-gray-2000 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
           {{ifExists ? "CONTINUAR" : "INICIAR" }}
         </button>
       </span>
@@ -119,27 +118,23 @@
               TEST DE COMPETENCIAS
             </h3>
                 <!-- INSTRUCCIONES -->
-            <p v-if="!this.isFinishedTest" class="text-xs select-none text-gray-500 text-justify border-b-2">Lea atentamente el enunciado y seleccione la opción que más se acople a su manera de ser. Si desea cerrar la ventana del test, se reiniciará por completo, por lo que, le pedimos atentamente que el test sea iniciado y terminado en el mismo intento.
-              DETERMINANTE:{{this.countA}}
-              INFLUENCIA:{{this.countB}}
-              ESTABILIDAD:{{this.countC}}
-              CUMPLIMIENTO:{{this.countD}}
+            <p v-if="!this.isFinishedTest" class="text-xs select-none text-gray-500 text-justify border-b-2 lg:mx-10 lg:text-base">Lea atentamente el enunciado y seleccione la opción que más se acople a su manera de ser. Si desea cerrar la ventana del test, se reiniciará por completo, por lo que, le pedimos atentamente que el test sea iniciado y terminado en el mismo intento.
             </p>
-            <p v-else class="text-xs text-gray-500 select-none text-justify border-b-2">¡Gracias por realizar el test de competencias!, los resultados han sido guardados y están en espera a revisión. Puede continuar completando el resto de zonas para su CV.</p>
+            <p v-else class="text-xs text-gray-500 select-none text-justify border-b-2 lg:text-lg">¡Gracias por realizar el test de competencias!, los resultados han sido guardados y están en espera a revisión. Puede continuar completando el resto de zonas para su CV.</p>
           </div>
             <!-- CUERPO -->
             <div v-if="!this.isFinishedTest" class="mt-2 flex flex-col">
-              <h4> {{ this.questions[this.countQuestion]['question']}}</h4>
+              <h4 class="leading-none text-center lg:text-xl lg:font-bold"> {{ this.questions[this.countQuestion]['question']}}</h4>
               <div class="flex flex-wrap justify-center gap-11">
                 <div class="py-2 gap-7 flex flex-col justify-center sm:flex-row sm:py-16">
-                    <label :for="index" v-for="words, index in questions[this.countQuestion]['options']" :key="index" :class="{'bg-green-600 text-white' : index == this.questions[this.countQuestion]['best_word'], 'bg-red-600 text-white' : index == this.questions[this.countQuestion]['worst_word'], 'cursor-pointer' : !this.questions[this.countQuestion]['fullAnswers']}" class="uppercase select-none my-auto content-center text-center w-full border-2 p-3 my-4s">
+                    <label :for="index" v-for="words, index in questions[this.countQuestion]['options']" :key="index" :class="{'bg-green-600 text-white' : index == this.questions[this.countQuestion]['best_word'], 'bg-red-600 text-white' : index == this.questions[this.countQuestion]['worst_word'], 'cursor-pointer' : !this.questions[this.countQuestion]['fullAnswers']}" class="uppercase select-none my-auto content-center text-center w-full border-2 p-3 lg:p-8 lg:text-2xl lg:font-semibold">
                       <input @click="answered($event)" type="radio" :disabled="this.questions[this.countQuestion]['fullAnswers']" :id="index" class="hidden content-center" :value="index">{{words}}
                     </label>
                 </div>
               </div>
             </div>
             <div v-else class="mt-2 flex flex-col">
-              <h4 class="font-bold text-center text-xl">¡TEST FINALIZADO!</h4>
+              <h4 class="font-bold text-center text-xl lg:text-2xl">¡TEST FINALIZADO!</h4>
               <div class="flex flex-wrap justify-center">
                 <svg class="w-48 h-48 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path></svg>
               </div>
@@ -148,24 +143,24 @@
       </div>
     </div>
         <!-- BOTONES / FOOTER PAGE -->
-    <div class="bg-gray-50 px-4 py-6 sm:px-6 flex flex-row justify-between items-end content-center text-end">
-      <p v-if="!this.isFinishedTest" class="text-gray-900 text-lg items-end">{{this.countQuestion+1}}/28</p>
-      <span v-if="!this.isFinishedTest" class="mt-3 flex w-auto items-center align-middle rounded-md shadow-sm sm:mt-0 sm:w-auto">
-        <button @click="clean()" type="button" class="inline-flex justify-center w-full rounded-md border select-none border-gray-300 mx-2 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+    <div class="flex flex-col bg-gray-50 px-4 py-6 text-center items-center justify-between content-center sm:px-6 lg:items-end lg:flex-row lg:text-end">
+      <p v-if="!this.isFinishedTest" class="order-last mt-4 text-gray-900 text-base items-end lg:text-lg lg:order-first">{{this.countQuestion+1}}/28</p>
+      <span v-if="!this.isFinishedTest" class="mt-3 flex w-auto items-center rounded-md justify-between shadow-sm sm:mt-0 sm:w-auto lg:align-middle">
+        <button @click="clean()" type="button" class="inline-flex justify-center w-full rounded-md border select-none border-gray-300 mx-2 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5 lg:px-8 lg:py-3 lg:text-xl">
           Limpiar
         </button>
-        <button :disabled="this.countQuestion<=0" :class="{'opacity-40' : this.countQuestion<=0}" @click="this.countQuestion--" type="button" class=" inline-flex select-none justify-center w-full rounded-md border border-gray-300 mx-2 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+        <button :disabled="this.countQuestion<=0" :class="{'opacity-40' : this.countQuestion<=0}" @click="this.countQuestion--" type="button" class=" inline-flex select-none justify-center w-full rounded-md border border-gray-300 mx-2 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5 lg:px-8 lg:py-3 lg:text-xl">
           Atrás
         </button>
-        <button v-if="this.countQuestion<27" :disabled="!this.questions[this.countQuestion]['fullAnswers']" :class="{'opacity-40' : !this.questions[this.countQuestion]['fullAnswers']}" @click="nextCalculate()" type="button" class=" inline-flex justify-center w-full select-none rounded-md border border-gray-300 mx-2 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-          Next
+        <button v-if="this.countQuestion<27" :disabled="!this.questions[this.countQuestion]['fullAnswers']" :class="{'opacity-40' : !this.questions[this.countQuestion]['fullAnswers']}" @click="nextCalculate()" type="button" class=" inline-flex justify-center w-full select-none rounded-md border border-gray-300 mx-2 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5 lg:px-8 lg:py-3 lg:text-xl">
+          Siguiente
         </button>
-        <button v-else @click="nextCalculate()" :disabled="!this.questions[this.countQuestion]['fullAnswers']" :class="{'opacity-40' : !this.questions[this.countQuestion]['fullAnswers']}" type="button" class=" inline-flex justify-center w-full rounded-md border border-gray-300 mx-2 px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:text-white focus:outline-none focus:border-blue-300 hover:bg-red-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+        <button v-else @click="nextCalculate()" :disabled="!this.questions[this.countQuestion]['fullAnswers']" :class="{'opacity-40' : !this.questions[this.countQuestion]['fullAnswers']}" type="button" class=" inline-flex justify-center w-full rounded-md border border-gray-300 mx-2 px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:text-white focus:outline-none focus:border-blue-300 hover:bg-red-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5 lg:px-8 lg:py-3 lg:text-xl">
           Finalizar
         </button>
       </span>
-      <span v-else class="mt-3 flex w-auto items-center align-middle rounded-md shadow-sm sm:mt-0 sm:w-auto">
-        <button @click="isStartTest = false, ifExists = true" type="button" class=" inline-flex justify-center w-full rounded-md border border-gray-300 mx-2 px-4 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:text-white focus:outline-none focus:border-blue-300 hover:bg-blue-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+      <span v-else class="mt-3 flex w-auto items-center align-middle rounded-md shadow-sm sm:mt-0 sm:w-auto lg:self-end">
+        <button @click="isStartTest = false, ifExists = true" type="button" class=" inline-flex justify-center justify-items-end w-full rounded-md border border-gray-300 mx-2 px-4 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:text-white focus:outline-none focus:border-blue-300 hover:bg-blue-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5 lg:px-8 lg:py-3 lg:text-xl">
           Finalizar
         </button>
       </span>
