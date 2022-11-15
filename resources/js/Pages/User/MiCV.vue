66dfeccd78e0a6672d3f3de7d608abd05aebb75b
HUGO ANAYA <hugo.anaya@live.com>
2022-11-15 12:12:36 -0600

    fix progress aside layout

--- resources/js/Pages/User/MiCV.vue
+++ resources/js/Pages/User/MiCV.vue
@@ -1,14 +1,31 @@
 <template>
   <AppLayout title="Mi CV">
-    <div class="min-h-screen">
+    <div class="min-h-screen pt-16">
       <Notification :message="$page.props.flash.message"
         class="text-green-500 font-semibold rounded-full border-green-800 fixed right-2 bottom-2" />
       <div class="flex items-start justify-start gap-4">
-        <Progress :userProp="user" :updateStatus="updateStatus" @sending-event="ReceivedEvent" class="hidden md:block"></Progress>
+        <Progress :userProp="user" :updateStatus="updateStatus" @sending-event="ReceivedEvent" class="md:block fixed"
+          :class="{ 'block': progressHide, 'hidden': !progressHide }"></Progress>
+        <div class="fixed inset-0 bg-gray-600 bg-opacity-75" :class="{ 'block': progressHide, 'hidden': !progressHide }">
+        </div>
+        <div class="hidden md:block invisible w-full md:max-w-[200px] lg:max-w-[220px] 2xl:max-w-[250px]"></div>
+        <div @click="ProgressBind()" class="animate-bounce fixed bottom-0 left-0 m-4 md:hidden z-50">
+          <svg :class="{ 'hidden': progressHide }" class="w-12 h-12 text-blue-600" fill="#fff" stroke="currentColor"
+            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+          </svg>
+          <svg :class="{ 'hidden': !progressHide }" class="w-12 h-12 text-red-600" fill="#fff" stroke="currentColor"
+            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path>
+          </svg>
+        </div>
         <!-- <Progress class="hidden md:block md:fixed md:top-20 md:left-0"></Progress> -->
         <div class="flex flex-col align-center gap-4 w-full align-center pb-3">
           <div class="flex flex-col align-center justify-center mx-3 mt-3 gap-4 lg:flex-row lg:mr-4 lg:mx-0">
-            <WorkPreferences :userProp="user" class="order-last shadow-lg shadow-gray-300 lg:order-first">
+            <WorkPreferences :userProp="user" @sending-event="ReceivedEvent"
+              class="order-last shadow-lg shadow-gray-300 lg:order-first">
             </WorkPreferences>
             <BasicInfo :userProp="user" @sending-event="ReceivedEvent"
               class="order first items-center shadow-lg shadow-gray-300 lg:order-first">
@@ -18,20 +35,21 @@
             <Study :userProp="user" @sending-event="ReceivedEvent" class="shadow-lg shadow-gray-300"></Study>
           </div>
           <div class="gap-4 mx-3 lg:mx-0 lg:mr-4">
-            <Certifications :userProp="user" class="shadow-lg shadow-gray-300"></Certifications>
+            <Certifications :userProp="user" @sending-event="ReceivedEvent" class="shadow-lg shadow-gray-300">
+            </Certifications>
           </div>
           <div class="gap-4 mx-3 lg:mx-0 lg:mr-4">
-            <Languages :userProp="user" class="shadow-lg shadow-gray-300"></Languages>
+            <Languages :userProp="user" @sending-event="ReceivedEvent" class="shadow-lg shadow-gray-300"></Languages>
           </div>
           <div class="gap-4 mx-3 lg:mx-0 lg:mr-4">
-            <Experience :userProp="user" class="shadow-lg shadow-gray-300"></Experience>
+            <Experience :userProp="user" @sending-event="ReceivedEvent" class="shadow-lg shadow-gray-300"></Experience>
           </div>
           <div class="gap-4 mx-3 lg:mx-0 lg:mr-4">
             <Skills :userProp="user" @sending-event="ReceivedEvent" class="shadow-lg shadow-gray-300"></Skills>
           </div>
           <div class="gap-4 mx-3 flex flex-col md:flex-row lg:mx-0 lg:mr-4">
-            <Testdisc :userProp="user" class="shadow-lg shadow-gray-300"></Testdisc>
-            <TestSoft :userProp="user" class="shadow-lg shadow-gray-300"></TestSoft>
+            <Testdisc :userProp="user" @sending-event="ReceivedEvent" class="shadow-lg shadow-gray-300"></Testdisc>
+            <TestSoft :userProp="user" @sending-event="ReceivedEvent" class="shadow-lg shadow-gray-300"></TestSoft>
           </div>
         </div>
       </div>
@@ -84,13 +102,18 @@ export default defineComponent({
   },
   data() {
     return {
-      updateStatus: ""
+      updateStatus: "",
+      progressHide: false
 
     }
   },
   methods: {
     ReceivedEvent(n) {
       this.updateStatus = n
+    },
+    ProgressBind() {
+      this.progressHide = !this.progressHide
+      console.log('ahora es: ' + this.progressHide)
     }
   }
 })
