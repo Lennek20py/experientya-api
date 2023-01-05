0bcf39b11017a0a02626815ace866ef21e5e5ab7
HUGO ANAYA <hugo.anaya@live.com>
2023-01-05 16:13:06 -0600

    hotfix tests wasnt working correctly, changes made it

--- resources/js/Pages/User/UserCV/Progress.vue
+++ resources/js/Pages/User/UserCV/Progress.vue
@@ -119,7 +119,7 @@
                   <a href="#skillsSection">
                      <span class="flex flex-row justify-between">Test Competencias
                         <svg class="w-6 h-6 text-yellow-500"
-                           :class="[progress['testcompetition'] == 'in_course' || progress['testcompetition'] <= 26 ? 'visible' : 'invisible']"
+                           :class="[((progress['testcompetition'] == 'in_course' && progress['testcompetition'] != null ) || (progress['testcompetition'] <= 26 && progress['testcompetition'] != null) ) ? 'visible' : 'invisible']"
                            fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z">
@@ -138,10 +138,11 @@
             <li>
                <div
                   class="p-1 lg:p-1.5 text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-blue-500 hover:text-white hover:cursor-pointer">
+                  {{ progress }}
                   <a href="#skillsSection">
                      <span class="flex flex-row justify-between">Test de SoftSkills
                         <svg class="w-6 h-6 text-yellow-500"
-                           :class="[progress['testsoftskill'] == 'in_course' || progress['testsoftskill'] <= 28 ? 'visible' : 'invisible']"
+                           :class="[((progress['testsoftskill'] == 'in_course' && progress['testsoftskill'] != null) || (progress['testsoftskill'] <= 28 && progress['testsoftskill'] != null) ) ? 'visible' : 'invisible']"
                            fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z">
