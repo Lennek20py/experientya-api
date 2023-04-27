8c519a3cea5b0ede2bd931b558bbe1ebc959209f
Francisco Javier <90472757+RikiRikin@users.noreply.github.com>
2023-04-26 20:11:07 -0600

    Update validation data - UserData.vue
    Faltaba tomar en cuenta que el teléfono 2 es opcional, al iniciar sesión con un usuario que no tiene teleéfono 2, la app crasheaba,

--- resources/js/Pages/User/Profile/UserData.vue
+++ resources/js/Pages/User/Profile/UserData.vue
@@ -47,146 +47,6 @@
                 </div>
             </div>
         </div>
-        <!-- <h2 class="mt-12 text-3xl font-extrabold text-gray-900">Datos Personales</h2>
-        <div class="grid sm:grid-cols-2 lg:grid-cols-4 md:grid-cols-2 gap-4">
-            <div class="mt-1">
-                <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Nombre(s)
-                    </span>
-                </div>
-                
-                <input type="text"
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2.5 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" v-model="props.userData.user_first_name"
-                />
-                <span class="text-red-500 text-sm">Nombre no válido</span>
-            </div>
-            <div class="mt-1">
-                <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Apellidos
-                    </span>
-                </div>
-                    <input type="text"
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2.5 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm" v-model="props.userData.user_last_name"
-                />
-            </div>
-            <div class="mt-1">
-                <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">CURP
-                    </span>
-                </div>
-                    <input type="text"
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2.5 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                v-model="props.userData.CURP"/>
-            </div>
-        </div> 
-        <h2 class="mt-12 text-3xl font-extrabold text-gray-900">Datos de Contacto</h2>
-        <div class="grid sm:grid-cols-2 lg:grid-cols-4 md:grid-cols-2 gap-4">
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">País
-                    </span>
-                </div>
-                    <select v-model="props.userData.user_country_id" id="state"
-                        class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm">
-                        <option v-for="country in countrys" :key="country.id"
-                            :value="country.id">
-                            {{ country.name }}
-                        </option>
-                    </select>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Estado
-                    </span>
-                </div>
-                    <select v-model="props.userData.user_state_id" id="state"
-                        class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm">
-                        <option v-for="state in states" :key="state.id"
-                            :value="state.id">
-                            {{ state.name }}
-                        </option>
-                    </select>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Ciudad / Municipio
-                    </span>
-                </div>
-                    <select v-model="props.userData.user_city_id" id="state"
-                        class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm">
-                        <option v-for="town in towns" :key="town.id" :value="town.id">
-                            {{ town.name }}
-                        </option>
-                    </select>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Dirección
-                    </span>
-                </div>
-                    <input type="text"
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                v-model="props.userData.user_address"/>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Código Postal
-                    </span>
-                </div>
-                    <input type="text" 
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                v-model="props.userData.user_postal_code"/>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Teléfono 1
-                    </span>
-                </div>
-                    <input type="text"
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                v-model="props.userData.user_phone_number"/>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Teléfono 2
-                    </span>
-                </div>
-                    <input type="text" 
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                v-model="props.userData.user_second_phone_number"/>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Correo electrónico alternativo
-                    </span>
-                </div>
-                    <input type="text"
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                v-model="props.userData.email_alternative"/>
-            </div>
-            <div class="mt-1">
-                    <div class="z-10 mb-[-10px]">
-                    <span
-                        class=" text-sm font-medium text-slate-700 after:ml-0.5 ml-3 bg-white pl-2 pr-2">Fecha de nacimiento
-                    </span>
-                </div>
-                    <input type="date"
-                    class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                v-model="props.userData.user_date_of_birth"/>
-            </div>
-        </div> -->
-        
         <div class="flex justify-end mt-6">
             <button v-if="btnStatus" class="text-white bg-slate-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-not-allowed">
                 Guardar
@@ -322,7 +182,9 @@
                     }
                 }else if(item.nameInput === 'Teléfono 1' || item.nameInput === 'Teléfono 2'){
                 // Phone
-                    if (props.userData[item.value].length != 10) {
+                    if (!props.userData[item.value]) {
+                        item.showMessage = false;
+                    }else if (props.userData[item.value].length != 10) {
                         item.showMessage = true;
                         item.message = 'El teléfono solo debe de tener 10 números'
                         result = false
