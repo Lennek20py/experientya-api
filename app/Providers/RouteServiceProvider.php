f576e74f3fdd0de328a96e5bea2f0ae306a02d3d
Francisco Javier <fco.javiersotolopez@gmail.com>
2023-05-30 23:17:11 -0600

    profile picture preview
    -Cambio de ruta al iniciar sesión, de vacantes a mi CV
    -Se quitó el botón de vacantes y mensajes en el menú
    -Se agregó la previsualización de la imagen del perfil en el formulario de registro
    -Agregar la siguiente imagen dentro de la carpeta profile: https://drive.google.com/file/d/1Qm8TDjEMPWkNYM3RZNA-KMKgdBkoJZEG/view?usp=sharing

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/vacancies';
+    public const HOME = '/micv';
 
     public const COMPANY_HOME = '/company/dashboard';
     /**
