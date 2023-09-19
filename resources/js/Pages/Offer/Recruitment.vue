93134fedf6864c27b37b2b5f55bbf7d2037133d2
HUGO ANAYA <hugo.anaya@live.com>
2023-09-19 09:13:47 -0600

    new Recruitment design

--- resources/js/Pages/Offer/Recruitment.vue
+++ resources/js/Pages/Offer/Recruitment.vue
@@ -1,59 +1,62 @@
 <template>
     <admin-layout title="Proceso de Reclutamiento">
-        <div class="min-h-screen pt-2 px-2 scroll-smooth cursor-default">
+        <div class="min-h-screen px-2 scroll-smooth cursor-default">
             <div
-                class="w-full w-4xl bg-white rounded-lg border border-gray-200 shadow-m px-5 py-2 2xl:px-8 2xl:min-h-[204px] transition ease-in-out delasy-75">
-                <h2 class="py-5 text-6xl uppercase font-extrabold text-gray-900 text-center">Proceso de Reclutamiento</h2>
-                <div class="text-center text-3xl">Vacante Actual: {{ Offer.title }}</div>
-                <div class="w-3/4 py-10 mx-auto">
+                class="w-full w-4xl bg-white rounded-lg border border-gray-200 shadow-m px-5 py-2 2xl:px-8 2xl:min-h-screen transition ease-in-out delasy-75">
+                <h2 class="py-5 text-2xl capitalize font-semibold text-gray-900 text-center lg:text-4xl lg:font-extrabold lg:text-start ">Proceso de Reclutamiento</h2>
+                <!-- <div class="text-center text-3xl">Vacante Actual: {{ Offer.title }}</div> -->
+                <div class="w-3/4 pb-4 mx-auto lg:text-start lg:mx-0 lg:w-5/12">
                     <div class="flex">
                         <select id="states" v-model="ActualOffer.id"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-x-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                            class="rounded bg-white border border-gray-300 text-gray-900 text-sm rounded-x-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            @change="this.getOrganizationOffers">
                             <option selected>Elige una Vacante</option>
                             <option v-for="(offer) in Offers" :key="offer.id" :value="offer.id">{{ offer.title }}</option>
                         </select>
                     </div>
                 </div>
                 <div
-                    class="flex flex-row flex-nowrap justify-center align-middle items-center content-center py-10 w-full w-4xl bg-white rounded-lg border gap-5 border-gray-200 shadow-m px-5 2xl:px-8 2xl:min-h-[204px] transition ease-in-out delasy-75">
-                    <div class="w-1/2 border rounded-md">
-                        <h3 class="text-center uppercase text-4xl font-bold text-gray-900">SOLICITUDES ENVIADAS</h3>
-                        <!-- <p>{{ Invites }}</p> -->
+                    class="flex flex-row flex-wrap py-2 w-full min-h-screen w-4xl bg-[#F8F8F8] rounded-lg border gap-5 border-gray-200 shadow-m px-2 lg:flex-nowrap lg:py-10 2xl:px-8 transition ease-in-out delasy-75">
+                    <div class="w-full border-b lg:bg-white lg:border lg:rounded-3xl lg:p-4">
+                        <h3 class="text-center pb-1 capitalize text-base font-normal text-gray-900 lg:text-2xl lg:text-start lg:py-2 lg:px-5 2xl:font-semibold">Solicitudes Enviadas
+                        </h3>
                         <div v-if="Invites.length > 0">
                             <div v-for="(Invite, index) in Invites" :key="index"
-                                class="cursor-pointer py-4 bg-['f9fafb'] text-gray-700 rounded-lg p-4 flex items-center">
-                                <img class="w-16 h-16 rounded-full mr-4" :src="Invite.cvid.user.profile_photo_link"
+                                class="cursor-pointer py-4 bg-['f9fafb'] text-gray-700 rounded-lg p-4 flex items-center lg:border lg:rounded-lg lg:bg-[#F8F8F8] lg:my-3 lg:py-2">
+                                <img class="w-12 h-12 rounded-full mr-4 2xl:w-15 2xl:h-15" :src="Invite.cvid.user.profile_photo_link"
                                     alt="Imagen de Usuario">
                                 <div>
-                                    <h2 class="text-xl font-semibold capitalize">{{ Invite.cvid.user.user_first_name }} {{
+                                    <h2 class="text-base font-semibold capitalize xl:font-bold xl:text-xl">{{ Invite.cvid.user.user_first_name }} {{
                                         Invite.cvid.user.user_last_name }}</h2>
-                                    <p class="text-sm capitalize">{{ Invite.cvid.position }}</p>
+                                    <p class="text-xs capitalize xl:text-base">{{ Invite.cvid.position }}</p>
                                 </div>
-                                <div class="ml-auto">
-                                    <h2 class="text-xl font-semibold">{{ translatedStatus(Invite.status) }}</h2>
+                                <div class="ml-auto bg-green-600 border rounded-md lg:rounded-lg">
+                                    <h2 class="text-sm font-semibold text-white mx-2 lg:text-lg lg:mx-4">{{ translatedStatus(Invite.status) }}</h2>
                                 </div>
                             </div>
                         </div>
                         <p v-else class="mx-auto text-center">No se encontraron usuarios</p>
                     </div>
-                    <div class="w-1/2 border rounded-md">
-                        <h3 class="text-center uppercase text-4xl font-bold text-gray-900">SOLICITUDES RECIBIDAS</h3>
+                    <div class="w-full border-b lg:bg-white lg:border lg:rounded-3xl lg:p-4">
+                        <h3 class="text-center pb-1 capitalize text-base font-normal text-gray-900 lg:text-2xl lg:text-start lg:py-2 lg:px-5">Solicitudes Recibidas
+                        </h3>
                         <div v-if="Candidates.length > 0">
                             <div v-for="(Candidate, index) in Candidates" :key="index"
-                                class="cursor-pointer py-4 bg-['f9fafb'] text-gray-700 rounded-lg p-4 flex items-center">
-                                <img class="w-16 h-16 rounded-full mr-4" :src="Candidate.cvid.user.profile_photo_link"
+                            class="cursor-pointer py-4 bg-['f9fafb'] text-gray-700 rounded-lg p-4 flex items-center lg:border lg:rounded-lg lg:bg-[#F8F8F8] lg:my-3 lg:py-2">
+                                <img class="w-12 h-12 rounded-full mr-4 2xl:w-15 2xl:h-15" :src="Candidate.cvid.user.profile_photo_link"
                                     alt="Imagen de Usuario">
                                 <div>
-                                    <h2 class="text-xl font-semibold capitalize">{{ Candidate.cvid.user.user_first_name }} {{
+                                    <h2 class="text-base font-semibold capitalize xl:font-bold xl:text-xl">{{ Candidate.cvid.user.user_first_name }} {{
                                         Candidate.cvid.user.user_last_name }}</h2>
-                                    <p class="text-sm capitalize">{{ Candidate.cvid.position }}</p>
+                                    <p class="text-xs capitalize xl:text-base">{{ Candidate.cvid.position }}</p>
                                 </div>
-                                <div class="ml-auto">
-                                    <h2 class="text-xl font-semibold">{{ translatedStatus(Candidate.status) }}</h2>
+                                <div class="ml-auto bg-green-600 border rounded-md lg:rounded-lg">
+                                    <h2 class="text-sm font-semibold text-white mx-2 lg:text-lg lg:mx-4">{{ translatedStatus(Candidate.status)
+                                    }}</h2>
                                 </div>
                             </div>
                         </div>
-                        <p v-else class="mx-auto text-center">No se encontraron solicitudes</p>
+                        <p v-else class="mx-auto text-center">No se encontraron usuarios</p>
                     </div>
                 </div>
             </div>
@@ -96,6 +99,7 @@ export default {
                     this.ActualOffer.id = this.Offer.id
                     this.Offers = response.data
                     this.getAppliedOffers()
+                    this.getCandidateInvites()
                 }).catch((error) => {
                     console.log(error)
                 })
@@ -115,8 +119,6 @@ export default {
             }
             await axios.get(route('candidatesInvites.index'), { params })
                 .then((response) => {
-                    console.log('aqui')
-                    console.log(response.data.data)
                     this.Invites = response.data.data
                 }).catch((error) => {
                     console.log(error)
